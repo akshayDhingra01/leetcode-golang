84e8b5763d02c4f244b23b8504d266e90466c1a2
akshayDhingra01 <a4akshayd@gmail.com>
2024-01-11 23:46:35 +0530

    ADD: Solution"

--- two-pointers/0011-container-with-most-water.go
+++ two-pointers/0011-container-with-most-water.go
@@ -1,1 +1,29 @@
 // https://leetcode.com/problems/container-with-most-water/description/
+
+
+func calculateArea(low, high, first, second int) int {
+	if second > first {
+		return first * (high - low)
+	} else {
+		return second * (high - low)
+	}
+}
+
+func maxArea(height []int) int {
+	low_pointer := 0
+	high_pointer := len(height) - 1
+	max_area := 0
+	for low_pointer < high_pointer {
+		newSum := calculateArea(low_pointer, high_pointer, height[low_pointer], height[high_pointer])
+		if newSum > max_area {
+			max_area = newSum
+		}
+		if height[low_pointer] < height[high_pointer] {
+			low_pointer++
+		} else {
+			high_pointer--
+		}
+
+	}
+	return max_area
+}
