fe2e1dee0145af156cec2f5a521a437583b880a6
akshayDhingra01 <a4akshayd@gmail.com>
2024-01-12 15:51:54 +0530

    ADD: Solution

--- array-hashing/0217-contains-duplicate.go
+++ array-hashing/0217-contains-duplicate.go
@@ -1,1 +1,14 @@
 // https://leetcode.com/problems/contains-duplicate/description/
+
+func containsDuplicate(nums []int) bool {
+    hash := make(map[int]int)
+
+    for _, num := range nums {
+        _, ok := hash[num]
+        if ok {
+            return true
+        }
+        hash[num] = 1
+    }
+    return false
+}
