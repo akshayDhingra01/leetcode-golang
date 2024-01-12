f99624b802838ad925d0a3e140526faa5ca17dc0
akshayDhingra01 <a4akshayd@gmail.com>
2024-01-12 17:06:54 +0530

    ADD: Solution

---
+++ array-hashing/0001-two-sum.go
@@ -0,0 +1,14 @@
+func twoSum(nums []int, target int) []int {
+
+    hash := make(map[int] int) // hash map of Value to obtain and it's index
+    for index, num := range nums {
+        get, ok := hash[num]
+        if ok {
+            ans := []int {get, index}
+            return ans
+        } else {
+            hash[target - num] = index
+        }
+    }
+    return []int{-1,-1}
+}
